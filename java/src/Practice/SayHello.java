35370728291bcb5be195a8a807f86ae66c9a968f
shreeranga <shreeranga@shreeranga-Lenovo-G500>
2018-12-03 20:54:36 +0530

    committing Runnable Program

--- java/src/Practice/SayHello.java
+++ java/src/Practice/SayHello.java
@@ -4,7 +4,8 @@ public class SayHello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		Runnable run = () -> System.out.println("Hello , how are you ");
+		run.run();
 	}
 
 }
