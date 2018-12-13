aa95c40a5ea858edd159372e8a2044d0bcbdd64e
Shreeranga1 <shreeranga.venugopal@nttdata.com>
2018-12-13 14:53:32 +0530

    I added Good morning greting !!

--- java/src/Practice/SayHello.java
+++ java/src/Practice/SayHello.java
@@ -4,7 +4,7 @@ public class SayHello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		Runnable run = () -> System.out.println("Hello , HII");
+		Runnable run = () -> System.out.println("Hello , HII,good morning");
 		run.run();
 	}
 
